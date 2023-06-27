e68bb4fd88a018de2ec6d86b0bd9660fc0884ca2
simonescoca <simone.scoca@icloud.com>
2023-06-27 15:19:19 +0200

    created new main components

---
+++ src/components/BestSeller.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <section>
+
+    </section>
+
+</template>
+
+<script>
+    export default {
+        components: {
+
+        },
+
+        data() {
+            return {
+                
+            }
+        },
+
+        created() {
+            
+        },
+
+        mounted() {
+            
+        },
+
+        props: {
+
+        },
+
+        computed: {
+            
+        },
+
+        methods: {
+            
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
