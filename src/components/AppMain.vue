4790ca943eaf02b13c280f5d45d5e742fe508cf8
simonescoca <simone.scoca@icloud.com>
2023-06-27 11:43:53 +0200

    basic main components structure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,18 +1,38 @@
 <template>
 
     <main>
-        <div class="container">
-            
-        </div>
+        <FeaturedProducts />
+        <MainCollections />
+        <BestSeller />
+        <ShippingDiscount />
+        <NewArrivals />
+        <TeamMembers />
+        <FromOurBlog />
+        <BottomCategories />
     </main>
 
 </template>
 
 <script>
+    import FeaturedProducts from './FeaturedProducts.vue';
+    import MainCollections from './MainCollections.vue';
+    import BestSeller from './BestSeller.vue';
+    import ShippingDiscount from './ShippingDiscount.vue';
+    import NewArrivals from './NewArrivals.vue';
+    import TeamMembers from './TeamMembers.vue';
+    import FromOurBlog from './FromOurBlog.vue';
+    import BottomCategories from './BottomCategories.vue';
 
     export default {
         components: {
-
+            FeaturedProducts,
+            MainCollections,
+            BestSeller,
+            ShippingDiscount,
+            NewArrivals,
+            TeamMembers,
+            FromOurBlog,
+            BottomCategories
         },
 
         data() {
