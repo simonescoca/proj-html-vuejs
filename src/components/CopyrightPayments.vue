7a2566387ff22758ad0b171c478c1f2727cd3265
simonescoca <simone.scoca@icloud.com>
2023-06-28 16:44:09 +0200

    created new component

---
+++ src/components/CopyrightPayments.vue
@@ -0,0 +1,48 @@
+<template>
+
+    <section>
+        <div class="container">
+            
+        </div>
+    </section>
+
+</template>
+
+<script>
+
+    export default {
+        components: {
+
+        },
+
+        data() {
+            return {
+                
+            }
+        },
+
+        created() {
+            
+        },
+
+        mounted() {
+            
+        },
+
+        props: {
+
+        },
+
+        computed: {
+            
+        },
+
+        methods: {
+            
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
