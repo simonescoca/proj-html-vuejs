89244442f1a820b393e06e4aaba87d4e5b9ad4bc
simonescoca <simone.scoca@icloud.com>
2023-06-28 17:47:19 +0200

    added payments footer section

--- src/components/CopyrightPayments.vue
+++ src/components/CopyrightPayments.vue
@@ -2,7 +2,12 @@
 
     <section>
         <div class="container">
-            
+            <div class="text-center my_text">
+                © Copyright 2012 - 2023 | Avada Theme by ThemeFusion | All Rights Reserved | Powered by WordPress
+            </div>
+            <div class="text-center my_img-container">
+                <img src="https://avada.website/classic-shop/wp-content/uploads/sites/48/2015/09/payment_cards_footer.png" alt="payment types">
+            </div>
         </div>
     </section>
 
@@ -17,7 +22,7 @@
 
         data() {
             return {
-                
+
             }
         },
 
@@ -45,4 +50,14 @@
 
 <style lang="scss" scoped>
 
+    section {
+        background-color: #1e2124;
+        padding: 3rem 0;
+    }
+
+    .my_text {
+        color: white;
+        padding-bottom: 1.5rem;
+        font-size: .85rem;
+    }
 </style>
